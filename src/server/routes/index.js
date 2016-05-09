cc74629fe2db17309edbbaf4dedabb73057a5659
David Sudia <dsudia@gmail.com>
2016-05-09 11:52:30 -0600

    socket emits on user joining

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
 var knex = require('../../../db/knex');
 var passport = require('../lib/passport');
 var bcrypt = require('bcrypt');
 var helpers = require('../lib/helpers');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).send('This is a NEAP webserver you\'ve got here!');
 });
 
 module.exports = router;
--- src/server/routes/userRoutes/addUser.js
+++ src/server/routes/userRoutes/addUser.js
@@ -1,9 +1,11 @@
 var userQueries = require('../../../../queries/users.js');
+var io = require('socket.io');
 
 module.exports = function(req, res, next) {
   userQueries.addUser(req.body.username, req.body.password)
     .then(function(user) {
-      res.status(200).send(user);
+      res.status(200).send(user[0]);
+      io.sockets.emit(user[0].username + "just joined!");
     })
     .catch(function(err) {
       res.status(400).send({message: 'There was a problem creating the user.'});
