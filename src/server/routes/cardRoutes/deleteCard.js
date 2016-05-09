5b1b6481db6d4d718bdc60a8d69a3a3c4da517cc
David Sudia <dsudia@gmail.com>
2016-05-09 11:23:54 -0600

    wire up deleting a card

---
+++ src/server/routes/cardRoutes/deleteCard.js
@@ -0,0 +1,8 @@
+var cardQueries = require('../../../../queries/cards');
+
+module.exports = function(req, res, next) {
+  return cardQueries.deleteCard(req.params.cardId);
+  .then(function(card) {
+    res.status(200).send(card[0]);
+  });
+}
--- src/server/routes/cards.js
+++ src/server/routes/cards.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 var helpers = require('../lib/helpers');
 var getCards = require('./cardRoutes/getCards');
+var delCard = require('./cardRoutes/deleteCard');
 
 router.get('/:deckId', function(req, res, next) {
   getCards(req, res, next);
+});
+
+router.delete('/:cardId', function(req, res, next) {
+  delCard(req, res, next);
 });
 
 module.exports = router;
