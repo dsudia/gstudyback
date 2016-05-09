a90f00fd2b1b2a6082e01333882969bd16123b87
David Sudia <dsudia@gmail.com>
2016-05-09 11:18:12 -0600

    wire up deleting a deck

---
+++ src/server/routes/deckRoutes/deleteDeck.js
@@ -0,0 +1,8 @@
+var deckQueries = require('../../../../queries/decks');
+
+module.exports = function(req, res, next) {
+  return deckQueries.deleteDeck(req.params.deckId);
+  .then(function(deck) {
+    res.status(200).send(deck[0]);
+  });
+}
--- src/server/routes/decks.js
+++ src/server/routes/decks.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var helpers = require('../lib/helpers');
 var add = require('./deckRoutes/addDeck');
 var getDecks = require('./deckRoutes/getDecks');
+var delDeck = require('./deckRoutes/deleteDeck');
 
 router.get('/add/:userId', function(req, res, next) {
   add(req, res, next);
@@ -11,5 +12,9 @@ router.get('/add/:userId', function(req, res, next) {
 router.get('/:userId', function(req, res, next) {
   getDecks(req, res, next);
 });
+
+router.delete('/:deckId', function(req, res, next) {
+  delDeck(req, res, next);
+})
 
 module.exports = router;
