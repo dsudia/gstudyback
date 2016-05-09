4bebb518fc25e9ee375758d3243196f56483dcb8
David Sudia <dsudia@gmail.com>
2016-05-09 10:14:38 -0600

    login route wired up

---
+++ src/server/routes/userRoutes/loginUser.js
@@ -0,0 +1,11 @@
+var userQueries = require('../../../queries/users.js');
+
+module.exports = function(req, res, next) {
+  userQueries.getUserByUsername(req.body.username)
+    .then(function(user) {
+      res.status(200).send(user);
+    })
+    .catch(function(err) {
+      res.status(400).send({message: 'Incorrect username or password'});
+    })
+}
--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var helpers = require('../lib/helpers');
+var login = require('./userRoutes/loginUser');
 
-router.get('/', function(req, res, next) {
-
+router.get('/login', function(req, res, next) {
+  login(req, res, next);
 });
 
 module.exports = router;
