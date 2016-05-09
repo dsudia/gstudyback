29b169540413cb6baa87987c11361da1772ab80d
David Sudia <dsudia@gmail.com>
2016-05-09 10:34:47 -0600

    used the function I wrote

--- queries/users.js
+++ queries/users.js
@@ -6,11 +6,11 @@ var users = function() {
 module.exports = {
 
   getUserByUsername: function(username) {
-    return knex('users').where('username', username);
+    return users().where('username', username);
   },
 
   addUser: function(username, password) {
-    return knex('users').insert({
+    return users().insert({
       username: username,
       password: password
     });
