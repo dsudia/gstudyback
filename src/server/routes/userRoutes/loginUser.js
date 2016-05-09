fbd4cc53880e11198ab71358f971dd020d95a05c
David Sudia <dsudia@gmail.com>
2016-05-09 10:48:59 -0600

    login passes tests

--- src/server/routes/userRoutes/loginUser.js
+++ src/server/routes/userRoutes/loginUser.js
@@ -3,9 +3,11 @@ var userQueries = require('../../../../queries/users.js');
 module.exports = function(req, res, next) {
   userQueries.getUserByUsername(req.body.username)
     .then(function(user) {
-      res.status(200).send(user);
-    })
-    .catch(function(err) {
-      res.status(400).send({message: 'Incorrect username or password'});
+      console.log(user[0]);
+      if (user[0] === undefined) {
+        res.status(400).send({message: 'Incorrect username or password'});
+      } else {
+        res.status(200).send(user[0]);
+      }
     });
 }
--- test/serverTests.spec.js
+++ test/serverTests.spec.js
@@ -6,3 +6,39 @@ var knex = require('../db/knex');
 var should = chai.should();
 
 chai.use(chaiHttp);
+
+describe('API Routes', function() {
+
+  describe('login user', function() {
+
+    it('should respond with a 200 and the user on good request', function(done) {
+      chai.request(server)
+        .get('/users/login')
+        .send({username: 'bentheliar', password: '12345678'})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.have.property('id');
+          res.body.id.should.equal(1);
+          res.body.should.have.property('username');
+          res.body.username.should.equal('bentheliar');
+          done();
+        });
+    });
+
+    it('should respond with a message when the username or password is wrong', function(done) {
+      chai.request(server)
+      .get('/users/login')
+      .send({username: 'beentheliar', password: '12345678'})
+      .end(function(err, res) {
+        res.should.have.status(400);
+        res.should.be.json;
+        res.body.should.have.property('message');
+        res.body.message.should.equal('Incorrect username or password');
+        done();
+      });
+    });
+
+  });
+
+});
